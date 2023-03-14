cc58bcd83c92b489dad36fcf88d85de58361f76b
Phuc <mphuc1010@gmail.comm>
2023-03-14 21:29:30 +0700

    section 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Section3 from "@/components/Section3";
 import Section4 from "@/components/Section4";
+import Section5 from "@/components/Section5";
 import SectionTwo from "@/components/SectionTwo";
 import Image from "next/image";
 import { MdKeyboardArrowRight } from "react-icons/md";
@@ -50,6 +51,7 @@ export default function Home() {
         <SectionTwo />
         <Section3 />
         <Section4 />
+        <Section5 />
       </div>
     </div>
   );
