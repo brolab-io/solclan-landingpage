7a102ae3549d7baae1887bf0780f9e9b2dc0320f
Phuc <mphuc1010@gmail.comm>
2023-03-14 16:49:25 +0700

    Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,48 +7,50 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 export default function Home() {
   return (
     <div>
-      <div className="bg-gradient-to-r from-violet-500 to-fuchsia-500">
-        <div className="relative">
+      <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500'>
+        <div className='relative'>
           <Image
             src={"/assets/bg-banner.png"}
-            alt="Banner"
+            alt='Banner'
             width={1920}
             height={971}
-            className="object-cover w-full"
+            className='object-cover w-full'
             priority
           />
-          <div className="grid grid-cols-2 absolute top-0 left-0 right-0 bottom-0 px-3 container mx-auto">
-            <div className="flex flex-col justify-center items-start text-white font-bold text-5xl tracking-wider leading-normal">
+          <div className='grid 2xl:grid-cols-2 absolute top-0 left-0 right-0 bottom-0 px-3 container mx-auto'>
+            <div className='flex flex-col justify-center items-start text-white font-bold text-5xl tracking-wider leading-normal order-last 2xl:order-first'>
               <h2>On-chain Governance</h2>
               <h2>for your bussiness</h2>
-              <div className="flex space-x-5 mt-5">
-                <button className="bg-white text-black text-sm px-4 py-2 rounded-full hover:bg-white/90 transition-colors duration-300">
+              <div className='flex space-x-5 mt-5'>
+                <button className='bg-white text-black text-sm px-4 py-2 rounded-full hover:bg-white/90 transition-colors duration-300'>
                   Get start now
                 </button>
-                <button className="flex items-centertext-white text-sm px-4 py-2">
+                <button className='flex items-centertext-white text-sm px-4 py-2'>
                   Join telegram
-                  <span className="ml-2">
+                  <span className='ml-2'>
                     <MdKeyboardArrowRight size={24} />
                   </span>
                 </button>
               </div>
             </div>
-            <div className="flex justify-end items-center">
+            <div className='flex justify-end items-center'>
               <Image
                 src={"/assets/iphone.png"}
-                alt="Phone"
+                alt='Phone'
                 width={383}
                 height={665}
-                className="object-cover"
+                className='object-cover mt-[250px] 2xl:mt-0'
                 priority
               />
             </div>
           </div>
         </div>
       </div>
-      <SectionTwo />
-      <Section3 />
-      <Section4 />
+      <div className='mt-[1500px] lg:mt-0'>
+        <SectionTwo />
+        <Section3 />
+        <Section4 />
+      </div>
     </div>
   );
 }
--- src/components/Section3.tsx
+++ src/components/Section3.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 export default function Section3() {
   return (
     <section>
-      <div className="container mx-auto px-3">
-        <div className="grid grid-cols-2 w-full">
-          <div className="space-y-3">
-            <h3 className="text-purple-500">Investment DAO</h3>
-            <h2 className="text-4xl uppercase tracking-wide font-bold">
+      <div className='container mx-auto px-3 mt-[550px] 2xl:mt-0'>
+        <div className='grid 2xl:grid-cols-2 w-full'>
+          <div className='space-y-3'>
+            <h3 className='text-purple-500'>Investment DAO</h3>
+            <h2 className='text-4xl uppercase tracking-wide font-bold'>
               OUR USP
             </h2>
-            <p className="font-sans justify-start">
+            <p className='font-sans justify-start'>
               Solclan&apos;s USPs include on-chain governance, anti-rug
               protection, automated financial execution, and a mobile app. These
               features provide transparency, safety, efficiency, and convenience
@@ -17,54 +17,54 @@ export default function Section3() {
               for small investors, venture capitalists, and project founders in
               the Web3 space.
             </p>
-            <button className="bg-white text-black font-bold text-sm px-4 py-2 rounded-full hover:bg-white/90 transition-colors duration-300">
+            <button className='bg-white text-black font-bold text-sm px-4 py-2 rounded-full hover:bg-white/90 transition-colors duration-300'>
               Get start now
             </button>
           </div>
-          <div className="grid grid-cols-2 gap-5 px-5 max-w-full justify-center items-center">
-            <div className="flex justify-end">
-              <div className="bg-white/5 rounded-sm p-5 flex flex-col justify-center items-center w-52 h-52 -mt-10  space-y-3">
+          <div className='grid grid-cols-2 gap-5 px-5 max-w-full justify-center items-center mt-10 2xl:mt-0'>
+            <div className='flex justify-end'>
+              <div className='bg-white/5 rounded-sm p-5 flex flex-col justify-center items-center w-52 h-52 -mt-10  space-y-3'>
                 <Image
                   src={"/assets/bitcoin-encryption.png"}
-                  alt="bitcoin-encryption.png"
+                  alt='bitcoin-encryption.png'
                   width={76}
                   height={76}
-                  className="object-cover"
+                  className='object-cover'
                   priority
                 />
-                <h4 className="font-bold text-center">
+                <h4 className='font-bold text-center'>
                   On-chain <br />
                   governance
                 </h4>
               </div>
             </div>
-            <div className="flex justify-start">
-              <div className="bg-white/5 rounded-sm p-5 flex flex-col justify-center items-center w-52 h-52 mt-10 space-y-3">
+            <div className='flex justify-start'>
+              <div className='bg-white/5 rounded-sm p-5 flex flex-col justify-center items-center w-52 h-52 mt-10 space-y-3'>
                 <Image
                   src={"/assets/cryptocurrencies-going-up.png"}
-                  alt="cryptocurrencies-going-up.png"
+                  alt='cryptocurrencies-going-up.png'
                   width={76}
                   height={76}
-                  className="object-cover"
+                  className='object-cover'
                   priority
                 />
-                <h4 className="font-bold text-center">
+                <h4 className='font-bold text-center'>
                   Anti-rug
                   <br /> protection
                 </h4>
               </div>
             </div>
-            <div className="flex justify-end">
-              <div className="bg-white/5 rounded-sm p-5 flex flex-col justify-center items-center w-52 h-52 -mt-10  space-y-3">
+            <div className='flex justify-end'>
+              <div className='bg-white/5 rounded-sm p-5 flex flex-col justify-center items-center w-52 h-52 -mt-10  space-y-3'>
                 <Image
                   src={"/assets/withdraw-a.png"}
-                  alt="withdraw-a.png"
+                  alt='withdraw-a.png'
                   width={76}
                   height={76}
-                  className="object-cover"
+                  className='object-cover'
                   priority
                 />
-                <h4 className="font-bold text-center">
+                <h4 className='font-bold text-center'>
                   Non-
                   <br />
                   custodial
@@ -72,17 +72,17 @@ export default function Section3() {
                 </h4>
               </div>
             </div>
-            <div className="flex justify-start">
-              <div className="bg-white/5 rounded-sm p-5 flex flex-col justify-center items-center w-52 h-52  space-y-3">
+            <div className='flex justify-start'>
+              <div className='bg-white/5 rounded-sm p-5 flex flex-col justify-center items-center w-52 h-52  space-y-3'>
                 <Image
                   src={"/assets/bitcoin-wallet-a.png"}
-                  alt="bitcoin-wallet-a.png"
+                  alt='bitcoin-wallet-a.png'
                   width={76}
                   height={76}
-                  className="object-cover"
+                  className='object-cover'
                   priority
                 />
-                <h4 className="font-bold text-center">
+                <h4 className='font-bold text-center'>
                   Automated
                   <br /> financial
                   <br /> execution
